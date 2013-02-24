698918b4454ccc3f94636d554a3107983cdc309d
Michael Fox <mikef@socialgo.com>
2013-02-24 17:07:21 +0000

    Initial commit.

---
+++ lib/peerindex.rb
@@ -0,0 +1,58 @@
+require 'faraday'
+require 'json'
+
+module PeerIndex
+  
+  class << self
+    
+    attr_accessor :apiKey
+    
+    def new(apiKey)
+      @apiKey = apiKey
+    end
+    
+    def get(path, params)      
+          
+      response = connection.get(path) do |request|
+        request.params = params
+        request.params['api_key'] = @apiKey
+      end
+      
+      response.status
+      
+      JSON.parse response.body
+      
+    end
+    
+    def actorBasic(*args)
+      get("actor/basic", *args)
+    end
+    
+    def actorExtended(*args)
+      get("actor/extended", *args)
+    end
+    
+    def actorTopic(*args)
+      get("actor/topic", *args)
+    end
+    
+    def actorGraph(*args)
+      get("actor/graph", *args)
+    end    
+    
+  end  
+  
+  module Connection
+    def connection
+              
+      @connection ||= begin
+        conn = Faraday.new('https://api.peerindex.com/1/')
+        
+        conn
+      end
+    end
+  end
+  
+  extend Connection
+  
+end  
---
+++ peerindex.gemspec
@@ -0,0 +1,11 @@
+Gem::Specification.new do |s|
+  s.name    = %q{peerindex}
+  s.version = "1.0.0"  
+  s.authors = ["Michael Fox"]
+  s.email   = ["mike@mikefox.info"]
+  s.summary = %q{Provides an interface to the PeerIndex REST API.}
+  s.files = [
+    "lib/peerindex.rb"
+  ]
+  s.require_paths = ["lib"]
+end
